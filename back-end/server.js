75829916505d44d347cc590c3f504262e5a875cd
tabishrahmani <tabish.rahmani@hotmail.com>
2021-11-24 21:03:59 +0530

    Initialized backend and frontend.

---
+++ back-end/server.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server is running at port ${port}`))
